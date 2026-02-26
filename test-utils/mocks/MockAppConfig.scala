837f5822a844955dcb41390787f93d53586e86e8
Michal Janowicz <michal.janowicz@digital.voa.gov.uk>
2026-02-26 10:11:28 +0000

    NGR-4198 rebased onto main

---
+++ test-utils/mocks/MockAppConfig.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mocks
+
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import config.AppConfig
+import org.mockito.Mockito.*
+import org.scalatestplus.mockito.MockitoSugar
+
+trait MockAppConfig extends MockitoSugar {
+
+  val mockConfig: AppConfig = mock[AppConfig]
+
+  when(mockConfig.appName).thenReturn("hello-world-bridge-integration-frontend")
+  when(mockConfig.host).thenReturn("http://localhost:9000")
+  when(mockConfig.vmvAddressLookup).thenReturn("http://localhost:9301")
+  when(mockConfig.bridgeIntegrationStubHost).thenReturn("http://localhost:1303")
+  when(mockConfig.timeToLive).thenReturn("3")
+}
